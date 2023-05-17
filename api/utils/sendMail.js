abc2d09182640f7017a6876f668a6f19bb30d966
WalterIt <whiruba@gmail.com>
2023-05-17 04:42:26 -0300

    API => Updated sendMail Function

--- api/utils/sendMail.js
+++ api/utils/sendMail.js
@@ -1,24 +1,39 @@
 const nodemailer = require("nodemailer");
 
 const sendMail = async (options) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMPT_HOST,
-        port: process.env.SMPT_PORT,
-        service: process.env.SMPT_SERVICE,
-        auth:{
-            user: process.env.SMPT_MAIL,
-            pass: process.env.SMPT_PASSWORD,
-        },
-    });
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMPT_HOST,
+    port: process.env.SMPT_PORT,
+    service: process.env.SMPT_SERVICE,
 
-    const mailOptions = {
-        from: process.env.SMPT_MAIL,
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-    };
+    // Added this lines
+    // credentials: "",
+    // secure: false,
+    // useSSL: false,
+    // ca: "",
+    // END of Added this lines
 
-    await transporter.sendMail(mailOptions);
+    auth: {
+      user: process.env.SMPT_MAIL,
+      pass: process.env.SMPT_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.SMPT_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+
+    // Added this lines
+    credentials: "",
+    secure: false,
+    useSSL: false,
+    ca: "",
+    // END of Added this lines
+  };
+
+  await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendMail;
